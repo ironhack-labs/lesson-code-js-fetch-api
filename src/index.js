5cc4929d8bb115e8565859bb832e90aeb438770b
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 15:36:11 +0200

    Example - fetch and display content on the page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 "use strict";
 
 fetch('https://api.spacexdata.com/v4/launches')
-  .then((response) => { 
-    return response.json();
-  })
+  .then((response) => response.json())
   .then((data) => {
-    console.log("Parsed response", data);
-  })
-  .catch( (err) => console.log(err));
+
+  data.forEach((launchObj) => {
+    const patchImage = launchObj.links.patch.small;
+    const imgElement = document.createElement("img");
+
+    imgElement.setAttribute("src", patchImage);
+    imgElement.setAttribute("width", 200);
+    document.body.appendChild(imgElement);
+  });
+
+}).catch((err) => console.log(err));
